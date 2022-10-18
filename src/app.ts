46772a6349ffe4c39c8f98b128c87a18ddcdf9ca
Esteban Tejeda <etejedaw@hotmail.com>
2022-10-18 18:59:42 -0300

    fix(core): remove config dependecy from app

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,21 @@
 import express, { Express } from "express";
-import Index from "./config";
-import configEnum from "./config/config.enum";
 
 class App {
-	private readonly app: Express;
-	private readonly config: Index;
+	readonly #app: Express;
+	readonly #PORT: string;
 
-	constructor() {
-		this.app = express();
-		this.app.use(express.json());
-		this.config = new Index();
+	constructor(port: string) {
+		this.#PORT = port;
+		this.#app = express();
+		this.#app.use(express.json());
 	}
 
 	getApp(): Express {
-		return this.app;
+		return this.#app;
 	}
 
 	listen(): void {
-		const PORT = this.config.get(configEnum.PORT);
-		this.app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
+		this.#app.listen(this.#PORT);
 	}
 }
 export default App;
