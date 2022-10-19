ad8c6e370593d5be0b1b7f270106bbde6ed8b1b1
Esteban Tejeda <etejedaw@hotmail.com>
2022-10-19 00:28:19 -0300

    chore: add NODE_ENV into config module

--- src/config/config.enum.ts
+++ src/config/config.enum.ts
@@ -3,7 +3,8 @@ enum ConfigEnum {
 	DB_HOST = "DB_HOST",
 	DB_PORT = "DB_PORT",
 	DB_USERNAME = "DB_USERNAME",
-	DB_PASSWORD = "DB_PASSWORD"
+	DB_PASSWORD = "DB_PASSWORD",
+	NODE_ENV = "NODE_ENV"
 }
 
 export default ConfigEnum;
