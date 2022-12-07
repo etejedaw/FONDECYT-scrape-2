46e4a9f0af4e568927c53dcad355b0009f0060ab
Esteban Tejeda <etejedaw@hotmail.com>
2022-12-06 23:13:52 -0300

    fix(htmlExtractor): remove option parameter in Puppeteer launch

--- src/helpers/htmlExtractor/puppeteer/Puppeteer.ts
+++ src/helpers/htmlExtractor/puppeteer/Puppeteer.ts
@@ -3,7 +3,7 @@ import HtmlExtractor from "../htmlExtractor.intereface";
 
 class Puppeteer implements HtmlExtractor {
 	async get(url: string): Promise<string> {
-		const browser = await puppeteer.launch({ headless: false });
+		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
 		await page.goto(url);
 		return await page.content();
