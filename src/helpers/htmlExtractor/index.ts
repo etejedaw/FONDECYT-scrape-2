e4b8d12be5d6f9bdb13b391bf0127410d615c2e9
Esteban Tejeda <etejedaw@hotmail.com>
2022-12-06 22:18:46 -0300

    feature(htmlExtractor): create puppeteer module in helpers to extract html

---
+++ src/helpers/htmlExtractor/index.ts
@@ -0,0 +1,5 @@
+import Getter from "./Getter";
+import Puppeteer from "./puppeteer/Puppeteer";
+import RequestPromise from "./requestPromise/RequestPromise";
+
+export { Getter, Puppeteer, RequestPromise };
---
+++ src/helpers/htmlExtractor/puppeteer/Puppeteer.ts
@@ -0,0 +1,13 @@
+import puppeteer from "puppeteer";
+import HtmlExtractor from "../htmlExtractor.intereface";
+
+class Puppeteer implements HtmlExtractor {
+	async get(url: string): Promise<string> {
+		const browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+		await page.goto(url);
+		return await page.content();
+	}
+}
+
+export default Puppeteer;
