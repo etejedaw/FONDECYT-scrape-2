ef36c61357488807fc2b252a1728f4855971eefc
Esteban Tejeda <etejedaw@hotmail.com>
2023-06-28 00:00:26 -0400

    feat(helpers): create function to download files from urls

---
+++ src/helpers/downloadFile.helpers.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs";
+import { Readable } from "stream";
+import { finished } from "stream/promises";
+
+async function downloadFile(url: string, path: string): Promise<void> {
+	try {
+		const response = await fetch(url);
+		const body = response.body;
+
+		createTmpDirectory(path);
+		const file = fs.createWriteStream(path, { flags: "w" });
+
+		await finished(Readable.fromWeb(body as any).pipe(file));
+		file.close();
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function createTmpDirectory(directory: string): void {
+	const arrayPath = directory.split("/");
+	arrayPath.pop();
+	const path = arrayPath.join("/");
+	if (!fs.existsSync(path)) fs.mkdirSync(path);
+}
+
+export default downloadFile;
