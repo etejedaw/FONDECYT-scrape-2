e4a16ba85b307c92e1e1ba733060691088a110f7
Esteban Tejeda <etejedaw@hotmail.com>
2025-01-31 20:24:01 -0300

    chore: add status code in get request

--- src/api/routes/emergencia-desastres.route.ts
+++ src/api/routes/emergencia-desastres.route.ts
@@ -14,7 +14,7 @@ router.get("/:indicator", async (req, res) => {
 	const scrapeFactory = ScraperFactory.getInstance();
 	const scraper = scrapeFactory.get("emergencia-desastres");
 	const data = await scraper.init(indicator);
-	return res.json({ data });
+	return res.json({ data }).status(200);
 });
 
 export default router;
